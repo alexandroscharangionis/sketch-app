0341d12dc8f85565b5ffa98abc66667a28819867
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-02-14 12:54:05 +0200

    fixed loop bug (was generating 1 div instead of 32)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const squareDiv = document.createElement("div");
-squareDiv.className = "single-square";
-
 for (let i = 0; i <= 32; i++) {
-  document.getElementById("main").appendChild(squareDiv);
+  let squareDiv = document.createElement("div");
+  squareDiv.className = "single-square";
+  let mainDiv = document.getElementById("main");
+  mainDiv.appendChild(squareDiv);
 }
