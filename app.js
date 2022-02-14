5907c129f4da2f5305ff8d6e42488185b6617f57
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-02-14 13:30:40 +0200

    changed loop to add 1024 divs instead of 256

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-for (let i = 0; i <= 32; i++) {
+for (let i = 0; i < 1024; i++) {
   let squareDiv = document.createElement("div");
   squareDiv.className = "single-square";
   let mainDiv = document.getElementById("main");
