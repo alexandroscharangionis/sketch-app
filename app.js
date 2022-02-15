f8843dc83b115e300b4032a492775cf75b35297b
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-02-15 19:40:14 +0200

    some code comments

--- app.js
+++ app.js
@@ -19,13 +19,16 @@ function createSquares() {
     square.style.width = `${calculateWidth}rem`;
     square.style.height = `${calculateHeight}rem`;
     grid.appendChild(square);
+
     square.addEventListener("mouseenter", () => {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      square.style.backgroundColor = `${color}`;
+      // const letters = "0123456789ABCDEF";
+      // let color = "#";
+
+      // for (let i = 0; i < 6; i++) {
+      //   color += letters[Math.floor(Math.random() * 16)];
+      //   square.style.backgroundColor = `${color}`;
+      // }
+      square.style.backgroundColor = `#000`;
     });
   }
 }
