80cda5df2816080ed46fcb9d5a73f5429b725a73
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-02-15 11:56:09 +0200

    added min and max value for user input

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const grid = document.querySelector("#main");
 
 function createSquares() {
   grid.innerHTML = "";
-  let input = document.getElementById("userInput").value;
+  let input = Number(document.getElementById("userInput").value);
+  if (input > 100 || input < 0) {
+    console.log("no");
+    return;
+  }
+
   grid.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
   for (let i = 0; i < input * input; i++) {
     const square = document.createElement("div");
@@ -16,7 +21,6 @@ function createSquares() {
 }
 
 const btn = document.getElementById("btn");
-
 btn.addEventListener("click", createSquares);
 
 let input = document.getElementById("userInput");
